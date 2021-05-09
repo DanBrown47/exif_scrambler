9f1fd9034695c36743cb316ad83d1bf1aa7e0ff6
DanBrown47 <danwand47@gmail.com>
2021-05-09 17:36:18 +0530

    Two more functions

--- core/scrambler.py
+++ core/scrambler.py
@@ -1,7 +1,15 @@
 from exif import Image
+import random, string
 
 class scrambler():
-    def random(filename):
+
+    def random():
+        x = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(16))
+        print(x)
+
+
+
+    def valid_exif_checker(filename):
         # Have a progress banner here
         with open(filename,'rb') as target_file:
             target = Image(target_file)
@@ -10,13 +18,24 @@ class scrambler():
             print("Has Exif")
 
             exif_list = []
+            exif_error = []
+            values = []
             exif_list = dir(target)
             for i in range(len(exif_list)):
-                user_comment = exif_list[i]
-                print(type(target))
-            # for i in range(20):
-            #     head = exif_list[i]
-            #     print(head)
-            #     # print(target.x)
-        else:
-            print("Dosent have exif")
+                key = exif_list[i]
+                try:
+                    values.append(getattr(target, key))
+                except Exception as e:
+                    exif_error.append(key)
+                    # Ad
+                    #d debuger
+            # print(exif_error)  ## Error
+            clean_arr = [i for i in exif_list if i not in exif_error]
+            return clean_arr
+    def main(filename):
+        clean_exif = scrambler.valid_exif_checker(filename)
+        scrambler.random()
+
+
+
+
